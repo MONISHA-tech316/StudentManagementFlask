a4ebb7bc28f52fdc63f8bd6ec43964f106494abd
POOJA-TECH485 <poojalingan637@gmail.com>
2026-06-23 12:42:17 +0530

    Fix register route

--- app.py
+++ app.py
@@ -6,10 +6,9 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
-
 @app.route("/register")
 def register():
-    return render_template("register.html")
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
